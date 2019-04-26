13b69ee21603befac7d1850bf2611da58bd7ca6d
Przemyslaw <przemyslaw.pukala@op.pl>
2019-04-26 18:56:22 +0200

    Update factoryMethod.cpp

--- factoryMethod.cpp
+++ factoryMethod.cpp
@@ -3,39 +3,39 @@
 class Gun
 {
 public:
-	virtual void fire() = 0;
+  virtual void fire() = 0;
 };
 
 class AK47 : public Gun
 {
 public:
-	void fire() override
-	{
-		std::cout << "tu tu tu tu tu\n";
-	}
+  void fire() override
+  {
+     std::cout << "tu tu tu tu tu\n";
+  }
 };
 
 class CreatorGun
 {
 public:
-	virtual Gun* createGun() = 0;
+  virtual Gun* createGun() = 0;
 };
 
 class CreatorAK46 : public CreatorGun
 {
 public:
-	Gun* createGun() override
-	{
-		return new AK47();
-	}
+  Gun* createGun() override
+  {
+    return new AK47();
+  }
 };
 
 int main()
 {
-	CreatorGun* creator = new CreatorAK46();
-	Gun* gun = creator->createGun();
+  CreatorGun* creator = new CreatorAK46();
+  Gun* gun = creator->createGun();
 
-	gun->fire();
-	delete creator;
-	delete gun;
+  gun->fire();
+  delete creator;
+  delete gun;
 }
